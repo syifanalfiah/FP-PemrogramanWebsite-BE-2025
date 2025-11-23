eec5e432998ceacdcac41d078b180b2e97b6c8f7
afif1731 <afifsdi@gmail.com>
2025-11-24 00:44:09 +0700

    feat: optional option in validate auth middleware

--- src/common/middleware/auth.middleware.ts
+++ src/common/middleware/auth.middleware.ts
@@ -11,9 +11,11 @@ import { ErrorResponse } from '../response';
 export const validateAuth =
   ({
     enable = true,
+    optional = false,
     allowed_roles,
   }: {
     enable?: boolean;
+    optional?: boolean;
     allowed_roles?: ROLE[];
   }) =>
   async (request: AuthedRequest, _: Response, next: NextFunction) => {
@@ -25,51 +27,54 @@ export const validateAuth =
       const requestToken = request.headers['authorization']?.split(' ');
 
       if (
-        !requestToken ||
-        requestToken[0].toLowerCase() !== 'bearer' ||
-        !requestToken[1]
+        !optional &&
+        (!requestToken ||
+          requestToken[0].toLowerCase() !== 'bearer' ||
+          !requestToken[1])
       )
         throw new ErrorResponse(StatusCodes.UNAUTHORIZED, 'Token required');
 
-      const payload = JwtUtils.verifyToken(requestToken[1]);
+      if (requestToken) {
+        const payload = JwtUtils.verifyToken(requestToken[1]);
 
-      if (!payload)
-        throw new ErrorResponse(StatusCodes.FORBIDDEN, 'Invalid token');
+        if (!payload)
+          throw new ErrorResponse(StatusCodes.FORBIDDEN, 'Invalid token');
 
-      const userData = await prisma.users.findUnique({
-        where: {
-          id: payload.user_id,
-          role: payload.role,
-        },
-        select: {
-          id: true,
-          email: true,
-          role: true,
-        },
-      });
+        const userData = await prisma.users.findUnique({
+          where: {
+            id: payload.user_id,
+            role: payload.role,
+          },
+          select: {
+            id: true,
+            email: true,
+            role: true,
+          },
+        });
 
-      if (!userData)
-        throw new ErrorResponse(
-          StatusCodes.FORBIDDEN,
-          'Invalid token, user not found',
-        );
+        if (!userData)
+          throw new ErrorResponse(
+            StatusCodes.FORBIDDEN,
+            'Invalid token, user not found',
+          );
 
-      if (
-        allowed_roles &&
-        userData.role !== 'SUPER_ADMIN' &&
-        ![...allowed_roles].some(role => userData.role.includes(role))
-      ) {
-        throw new ErrorResponse(
-          StatusCodes.FORBIDDEN,
-          'This role is not allowed to access the endpoint',
-        );
+        if (
+          allowed_roles &&
+          userData.role !== 'SUPER_ADMIN' &&
+          ![...allowed_roles].some(role => userData.role.includes(role))
+        ) {
+          throw new ErrorResponse(
+            StatusCodes.FORBIDDEN,
+            'This role is not allowed to access the endpoint',
+          );
+        }
+
+        request.user = {
+          user_id: userData.id,
+          email: userData.email,
+          role: userData.role,
+        };
       }
-
-      request.user = {
-        user_id: userData.id,
-        email: userData.email,
-        role: userData.role,
-      };
 
       next();
     } catch (error) {
