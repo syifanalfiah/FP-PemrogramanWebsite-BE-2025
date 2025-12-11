05cccab447045a0ba78ee595be5f5f7bdc9056d4
syifanalfiah <112307944+syifanalfiah@users.noreply.github.com>
2025-12-11 12:55:39 +0700

    Fix export statements in games index file

--- src/common/interface/games/index.ts
+++ src/common/interface/games/index.ts
@@ -1,6 +1,4 @@
 export * from './pair-or-no-pair.interface';
-export * from './pair-or-no-pair.interface';
 export * from './quiz.interface';
-export * from './quiz.interface';
-export * from './type-speed.interface';
 export * from './speed-sorting.interface';
+export * from './type-speed.interface';
